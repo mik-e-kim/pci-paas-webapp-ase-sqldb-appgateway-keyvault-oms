fa018262067d28ffb86dd5db86bad967bf4ff686
Michael Kim <33297027+mik-e-kim@users.noreply.github.com>
2018-06-21 08:53:27 -0700

    Updating script
    Script will no longer capture cleartext credentials for logging into Azure. Implemented input parameter validation, as well as introducing updated error messaging for guiding users through troubleshooting.

--- 2-EnableOMSLoggingOnResources.ps1
+++ 2-EnableOMSLoggingOnResources.ps1
@@ -3,58 +3,83 @@ This script enables diagnostics logging on the resources and configure Log analy
 
 Make sure you import AzureRM and AzureDiagnosticsAndLogAnalytics modules before executing this script.
 
-USAGE:
-    .\2-EnableOMSLoggingOnResources.ps1 -resourceGroupName demorg -globalAdminUserName globaladmin@azuredomain.com -globalAdminPassword *******
-    -subscriptionID xxxxxxx-f760-xxxx-bd98-xxxxxxxx
-
 #>
 
-[CmdletBinding()]
-param (
-        # Provide resourceGroupName for deployment
+# Login to AzureRM function
+function loginToAzureRM {
+	Param(
+		[Parameter(Mandatory=$true)]
+		[int]$loginCount,
         [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateLength(1,64)]
-        [ValidatePattern('^[\w]+$')]
-        [string]
-        $resourceGroupName,
+		[string]$subscriptionID
+	)
 
-        # Provide Azure AD UserName with Global Administrator permission on Azure AD and Service Administrator / Co-Admin permission on Subscription.
-        [Parameter(Mandatory=$True)] 
-        [string]$globalAdminUserName, 
+    # Login to AzureRM Service
+    Write-Host -ForegroundColor Yellow "`t* Prompt for connecting to AzureRM Subscription - $subscriptionID."
+    Login-AzureRmAccount -SubscriptionId $subscriptionID | Out-null
+    if (Get-AzureRmContext) {
+        Write-Host -ForegroundColor Cyan "`t`t-> Connection to AzureRM Subscription established successfully for managing Azure Resource Manager."
+    }
 
-        # Provide password for Azure AD UserName.
-        [Parameter(Mandatory=$True)] 
-        [string]$globalAdminPassword,
+    # Login Validation
+	if($?) {
+		Write-Host "`t`t`t*** Azure Resource Manager (ARM) Login Successful! ***" -ForegroundColor Green
+	} 
+    else {
+		if ($loginCount -lt 3) {
+			$loginCount = $loginCount + 1
+			Write-Host -ForegroundColor Magenta "`t-> Invalid Credentials! Please try logging in again."
+			loginToAzure -lginCount $loginCount
+		} 
+        else {
+			Write-Host -ForegroundColor Magenta "`t-> Credentials input are incorrect, invalid, or exceed the maximum number of retries. Verify the correct Azure account information is being used."
+			Write-Host -ForegroundColor Yellow "                                    Press any key to exit..."
+			$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+			Exit
+		}
+	}
+}
 
-        # Provide Subscription ID that will be used for deployment
-        [Parameter(Mandatory=$true)]
-        [string]
-        [ValidateNotNullOrEmpty()]
-        $subscriptionID
+    # Provide resourceGroupName for deployment
+    Write-Host -ForegroundColor Yellow " Name of the resource group to be created for this deployment (if blank, 'PCI-PayProc-BP' will be used as the resource group name)."
+    $resourceGroupName = Read-Host " Resource Group Name"
+    if ($resourceGroupName -eq "") {$resourceGroupName = "PCI-PayProc-BP"}
+    Write-Host ""
 
-)
+    # Provide Subscription ID that will be used for deployment
+    Write-Host -ForegroundColor Yellow " Azure Subscription ID associated to the Global Administrator account for deploying this solution." 
+    do {
+        try {
+            [System.Guid]$subscriptionID = Read-Host " Azure Subscription ID"
+        }
+        catch {
+            Write-Host -ForegroundColor Magenta "`t-> Please enter a valid Azure Subscription ID."
+        }
+    }
+    until (
+        $subscriptionID -match ("^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$")
+    )
+    Write-Host ""
+
+### Login to Azure RM ###
+Write-Host -ForegroundColor Green "`n###############################         Connecting to Azure        ############################### `n "
 
 try {
-    
-    Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted -Force
-    
-    # Creating a Login credential.
-    $secpasswd = ConvertTo-SecureString $globalAdminPassword -AsPlainText -Force
-    $psCred = New-Object System.Management.Automation.PSCredential ($globalAdminUserName, $secpasswd)
+    loginToAzureRM -loginCount 1 -subscriptionID $subscriptionID
+}
+catch {
+    Write-Host -ForegroundColor Magenta "`t-> Azure login attempts failed. Verify your user credentials before running the deployment script again."
+    Break
+}
 
-    # Connecting to Azure Subscription
-    Write-Host -ForegroundColor Yellow "`nConnecting to AzureRM Subscription - $subscriptionID."
-    Login-AzureRmAccount -Credential $psCred -SubscriptionId $subscriptionID | Out-null
-    if(Get-AzureRmContext){
-        Write-Host -ForegroundColor Yellow "`t* Connection to AzureRM Subscription established successfully."
-    }
+try {
+    Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted -Force
             
     # Start OMS Diagnostics
-    Write-Host ("`nGetting OMS Workspace details.." ) -ForegroundColor Yellow
+    Write-Host ("`n Getting OMS Workspace details.." ) -ForegroundColor Yellow
     $omsWS = Get-AzureRmOperationalInsightsWorkspace -ResourceGroupName $ResourceGroupName
 
-    Write-Host ("`nCollecting list of resourcetype to enable log analytics." ) -ForegroundColor Yellow
+    Write-Host ("`n Collecting list of resourcetype to enable log analytics." ) -ForegroundColor Yellow
     $resourceTypes = @( "Microsoft.Network/applicationGateways",
                         "Microsoft.Network/NetworkSecurityGroups",
                         "Microsoft.Web/serverFarms",
@@ -63,14 +88,14 @@ try {
                         "Microsoft.Web/sites",
                         "Microsoft.KeyVault/Vaults" )
 
-    Write-Host ("`nEnabling diagnostics for each resource type." ) -ForegroundColor Yellow
+    Write-Host ("`n Enabling diagnostics for each resource type." ) -ForegroundColor Yellow
     foreach($resourceType in $resourceTypes)
     {
         Enable-AzureRMDiagnostics -ResourceGroupName $resourceGroupName -SubscriptionId $subscriptionId -WSID $omsWS.ResourceId -ResourceType $resourceType -Force -Update -EnableLogs -EnableMetrics 
     }
 
     $workspace = Find-AzureRmResource -ResourceType "Microsoft.OperationalInsights/workspaces" -ResourceNameContains $omsWS.Name -ResourceGroupNameEquals $resourceGroupName
-    Write-Host ("`nConfigure Log Analytics to collect Azure diagnostic logs" ) -ForegroundColor Yellow
+    Write-Host ("`n Configure Log Analytics to collect Azure diagnostic logs" ) -ForegroundColor Yellow
     foreach($resourceType in $resourceTypes)
     {
         Write-Host ("`n`t-> Adding Azure Diagnostics to Log Analytics for -" + $resourceType) -ForegroundColor Yellow
@@ -80,6 +105,7 @@ try {
     # End OMS Diagnostics    
 }
 catch {
-    throw $_
+    Write-Host -ForegroundColor Magenta "`t-> Could not configure OMS for the specified resources. Verify the resources specified are available in the Azure portal before re-running this script."
+    Break
 }
 
